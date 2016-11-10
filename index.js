2334de11234ca5baf1d68724af2b1a09b13560c4
ksed <hydrogeo.coder@gmail.com>
2016-11-09 19:53:32 -0500

    added connection to the mongo db

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var server = express();
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var albumRouter = require('./server/routers/album.router.js');
+var mongoURI = process.env.MONGOURI || require('./config.js').mongoURI;
+
+mongoose.connect(mongoURI);
+//server.use(albumRouter);
+
+server.listen(port, function() {
+  console.log('Now listening on port: '+port);
+});
